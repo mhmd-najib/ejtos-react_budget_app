807e8649d3efdef05bb857f40dc14b1588aabe8a
mhmd-najib <mhmd.najib@gmail.com>
2023-02-11 05:36:03 -0500

    Stage 1

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,38 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses,budget,dispatch } = useContext(AppContext);
+
+    const checkBudget = (value) => {
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        if(value > 20000) {
+            alert("The value cannot exceed 20000");  
+        }
+        else if (value < totalExpenses) {
+            alert("Budget cannot be less than Expenses");
+        }
+        else {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload : value
+            })
+        }
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        step="10"
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => checkBudget(event.target.value)}>
+            </input>
         </div>
     );
 };
